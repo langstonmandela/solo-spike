a614b9c9c5c5185162a1890254dd7e5999b682b6
Riley DuBord <thelifeofdu@gmail.com>
2024-02-08 14:10:17 -0600

    most of the sagas are done

--- src/components/SearchView/Form.jsx
+++ src/components/SearchView/Form.jsx
@@ -1,6 +1,11 @@
 export default function Form () {
 
     return (
+        <>
         <h1>IN FROM</h1>
+        <form>
+            <input type="text" placeholder="SEARCH FOR GIFS"/>
+        </form>
+        </>
     )
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,68 +1,84 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
-import { takeEvery, put, take } from 'redux-saga/effects';
-import axios from 'axios';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { takeEvery, put, take } from "redux-saga/effects";
+import axios from "axios";
 
+const gifList = (state = [], action) => {
+  switch (action.type) {
+    case "DISPLAY_GIF":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const favoriteList = (state = [], action) => {
+  switch (action.type) {
+    case "DISPLAY_FAVS":
+      return action.payload;
+    case "ADD_FAV":
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
 
 function* rootSaga() {
-    yield takeEvery('FETCH_GIF', fetchGifSaga) // GET from giphy (with search params)
-    yield takeEvery('FETCH_FAV', fetchFavSaga) // GET from favorites table from db
-    yield takeEvery('ADD_FAV', addFavSaga) // POST fav to db from form
-    yield takeEvery('SET_CATERGORY', setCategorySaga) // PUT the category id in the fav table for the specific item
-    // yield takeEvery("DELETE_FAV", deleteFavSaga)
-  };
+  yield takeEvery("FETCH_GIF", fetchGifSaga); // GET from giphy (with search params)
+  yield takeEvery("FETCH_FAVS", fetchFavSaga); // GET from favorites table from db
+  yield takeEvery("POST_FAV", addFavSaga); // POST fav to db from form
+  yield takeEvery("SET_CATERGORY", setCategorySaga); // PUT the category id in the fav table for the specific item
+  // yield takeEvery("DELETE_FAV", deleteFavSaga)
+}
 
-
-  function* fetchGifSaga(action){
-    try {
-        const response = yield axios.get('')
-        console.log('respone', response.data);
-        yield put({type: '', payload: response.data})
-    } catch (error) {
-        // console.error('Error in FETCH saga', error)
-    }
+function* fetchGifSaga(action) {
+  try {
+    const response = yield axios.get("/api/search");
+    console.log("response", response.data);
+    yield put({ type: "DISPLAY_GIF", payload: response.data });
+  } catch (error) {
+    console.error("Error in FETCH saga", error);
   }
+}
 
-  function* fetchFavSaga(action){
-    try {
-        // const response = yield axios.get('/api/plants')
-        // console.log('respone', response.data);
-        // yield put({type: 'GET_PLANTS', payload: response.data})
-    } catch (error) {
-        // console.error('Error in FETCH saga', error)
-    }
+function* fetchFavSaga(action) {
+  try {
+    const response = yield axios.get("/api/favorites");
+    console.log("response", response.data);
+    yield put({ type: "DISPLAY_FAVS", payload: response.data });
+  } catch (error) {
+    console.error("Error in FETCH saga", error);
   }
+}
 
-  function* addFavSaga(action){
-    try {
-        // const response = yield axios.get('/api/plants')
-        // console.log('respone', response.data);
-        // yield put({type: 'GET_PLANTS', payload: response.data})
-    } catch (error) {
-        // console.error('Error in FETCH saga', error)
-    }
+function* addFavSaga(action) {
+  try {
+    const response = yield axios.post("/api/favorites");
+    console.log("respone", response.data);
+    yield put({ type: "ADD_FAV", payload: response.data });
+  } catch (error) {
+    console.error("Error in FETCH saga", error);
   }
+}
 
-  function* setCategorySaga(action){
-    try {
-        // const response = yield axios.get('/api/plants')
-        // console.log('respone', response.data);
-        // yield put({type: 'GET_PLANTS', payload: response.data})
-    } catch (error) {
-        // console.error('Error in FETCH saga', error)
-    }
+function* setCategorySaga(action) {
+  try {
+    // const response = yield axios.get('/api/plants')
+    // console.log('respone', response.data);
+    // yield put({type: 'GET_PLANTS', payload: response.data})
+  } catch (error) {
+    // console.error('Error in FETCH saga', error)
   }
+}
 
-
-  const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   combineReducers({ REDUCERHERE }),
-  applyMiddleware(sagaMiddleware, logger),
+  applyMiddleware(sagaMiddleware, logger)
 );
 
 sagaMiddleware.run(rootSaga);
-
 
 export default store;
