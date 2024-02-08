16f91c94f0f832897875449c72e92f800e70becb
Andrew Haarsager <athaarsager@gmail.com>
2024-02-08 15:22:11 -0600

    Added conditional rendering to FavList.jsx

--- src/components/FavView/FavList.jsx
+++ src/components/FavView/FavList.jsx
@@ -1,12 +1,19 @@
-import { useEffect, useSelector } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector} from "react-redux";
 
 export default function FavList () {
+    const dispatch = useDispatch();
     const favoriteList = useSelector(store => store.favoriteList);
+
+    useEffect(() => {
+        dispatch({type: "FETCH_FAVES"});
+    }, []);
     
     return (
         <>
         <h1>IN FavList</h1>
-        <ul>{favoriteList.map(gif => (
+        <ul>{favoriteList.length === 0 ? <h2>Loading...</h2> : 
+        favoriteList.map(gif => (
             <li key={gif.id}><img src={gif.gif_url} /></li>
         ))}</ul>
         </>
