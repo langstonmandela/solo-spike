fe774af95dd83a50497f9df1221f2900931105de
Riley DuBord <thelifeofdu@gmail.com>
2024-02-08 14:43:14 -0600

    redux and react redux and redux logger installed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,13 @@
 import FavList from "../FavView/FavList";
 import Form from "../SearchView/Form";
 import List from "../SearchView/List";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
 function App() {
   return (
