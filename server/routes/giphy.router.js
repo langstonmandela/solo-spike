634b9ce40e6e91b6c171559813263eb4ca3dd03b
Riley DuBord <thelifeofdu@gmail.com>
2024-02-08 11:56:01 -0600

    api key and search get set up

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const pool = require('../modules/pool');
-
 const router = express.Router();
+const GIPHY_API_KEY = process.env.GIPHY_API_KEY;
+
+router.get("/", (req, res) => {
+  const searchText = req.query.searchText
+  axios
+    .get(
+      `http://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${searchText}`
+      
+    )
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
