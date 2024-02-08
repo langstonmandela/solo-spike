84f974a7f9679c0da32fc0fc2f503590346dcc81
Andrew Haarsager <athaarsager@gmail.com>
2024-02-08 15:56:53 -0600

    Fixed dropdown to be made of buttons

--- src/components/FavView/FavList.jsx
+++ src/components/FavView/FavList.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function FavList() {
@@ -16,14 +16,16 @@ export default function FavList() {
                 favoriteList.map(gif => (
                     <div class="w3-dropdown-hover">
                         <li key={gif.id}><img src={gif.gif_url} /></li>
-                        <div>
+                        
+                            <div class="w3-container w3-light-grey">
+                                {/* button text conditionally renders based on what is in store. dropdowns set store */}
                             <button className="w3-button">Assign Catgory</button>
                             <div className="w3-dropdown-content w3-bar-block w3-border">
-                                <a href="#" class="w3-bar-item w3-button">Wild</a>
-                                <a href="#" class="w3-bar-item w3-button">Uproarious</a>
-                                <a href="#" class="w3-bar-item w3-button">Poignant</a>
-                                <a href="#" class="w3-bar-item w3-button">Felicitous</a>
-                                <a href="#" class="w3-bar-item w3-button">Whimsical</a>
+                                <button class="w3-bar-item w3-button">Wild</button>
+                                <button class="w3-bar-item w3-button">Uproarious</button>
+                                <button class="w3-bar-item w3-button">Poignant</button>
+                                <button class="w3-bar-item w3-button">Felicitous</button>
+                                <button class="w3-bar-item w3-button">Whimsical</button>
                             </div>
                         </div>
                     </div>
