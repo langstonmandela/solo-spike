d2472966b0ccbd9545eb02c84bd46cca2b564e7a
Seid Redi <seidredi7@gmail.com>
2024-02-08 15:54:54 -0600

    get gifList and pass each gif to list item as props

--- src/components/SearchView/List.jsx
+++ src/components/SearchView/List.jsx
@@ -1,11 +1,17 @@
 import ListItem from "./ListItem";
+import { useSelector } from 'react-redux';
 
 export default function List () {
+    const gifList = useSelector(s=>s.gifList);
+    console.log(gifList);
+    //data[0].images.original.url
 
     return (
         <>
         <h1>IN LIST</h1>
-        <ListItem />
+        {
+            gifList.map((gif,i)=><ListItem key={i} props={gif.data[0].images.original.url}/>)
+        }
         </>
     )
 }
