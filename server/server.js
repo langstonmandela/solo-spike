a4e137b814849888e5bb57c875c719c1da84672d
Riley DuBord <thelifeofdu@gmail.com>
2024-02-08 11:52:23 -0600

    giphy router initialized

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const favoriteRouter = require('./routes/favorite.router');
 const categoryRouter = require('./routes/category.router');
+const giphyRouter = require('./routes/giphy.router')
 const PORT = process.env.PORT || 5001;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -12,6 +13,7 @@ app.use(express.static('build'));
 /** ---------- EXPRESS ROUTES ---------- **/
 app.use('/api/favorites', favoriteRouter);
 app.use('/api/categories', categoryRouter);
+app.use('/api/search', giphyRouter)
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -6,13 +6,54 @@ import axios from 'axios';
 
 
 function* rootSaga() {
-    yield takeEvery('FETCH_GIF', fetchGifSaga) // GET from giphy
+    yield takeEvery('FETCH_GIF', fetchGifSaga) // GET from giphy (with search params)
     yield takeEvery('FETCH_FAV', fetchFavSaga) // GET from favorites table from db
     yield takeEvery('ADD_FAV', addFavSaga) // POST fav to db from form
-    yield takeEvery('SET_CATERGORY', setCategorySaga)
-
+    yield takeEvery('SET_CATERGORY', setCategorySaga) // PUT the category id in the fav table for the specific item
     // yield takeEvery("DELETE_FAV", deleteFavSaga)
   };
+
+
+  function* fetchGifSaga(action){
+    try {
+        const response = yield axios.get('')
+        console.log('respone', response.data);
+        yield put({type: '', payload: response.data})
+    } catch (error) {
+        // console.error('Error in FETCH saga', error)
+    }
+  }
+
+  function* fetchFavSaga(action){
+    try {
+        // const response = yield axios.get('/api/plants')
+        // console.log('respone', response.data);
+        // yield put({type: 'GET_PLANTS', payload: response.data})
+    } catch (error) {
+        // console.error('Error in FETCH saga', error)
+    }
+  }
+
+  function* addFavSaga(action){
+    try {
+        // const response = yield axios.get('/api/plants')
+        // console.log('respone', response.data);
+        // yield put({type: 'GET_PLANTS', payload: response.data})
+    } catch (error) {
+        // console.error('Error in FETCH saga', error)
+    }
+  }
+
+  function* setCategorySaga(action){
+    try {
+        // const response = yield axios.get('/api/plants')
+        // console.log('respone', response.data);
+        // yield put({type: 'GET_PLANTS', payload: response.data})
+    } catch (error) {
+        // console.error('Error in FETCH saga', error)
+    }
+  }
+
 
   const sagaMiddleware = createSagaMiddleware();
 
