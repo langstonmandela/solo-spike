f56117078406f2325611a9d15e44269a8196465a
Andrew Haarsager <athaarsager@gmail.com>
2024-02-08 14:33:25 -0600

    Began creating FavList.jsx

--- src/components/FavView/FavList.jsx
+++ src/components/FavView/FavList.jsx
@@ -1,6 +1,12 @@
+import { useEffect, useSelector } from "react";
+
 export default function FavList () {
+    
 
     return (
+        <>
         <h1>IN FavList</h1>
+        <ul></ul>
+        </>
     )
 }
