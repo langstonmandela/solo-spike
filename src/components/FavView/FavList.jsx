6da7e1d692c8b26e1eecdd2869a83451ded2547f
Andrew Haarsager <athaarsager@gmail.com>
2024-02-08 15:42:34 -0600

    Created drop-down button for categories. Does not work yet

--- src/components/FavView/FavList.jsx
+++ src/components/FavView/FavList.jsx
@@ -1,22 +1,33 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-export default function FavList () {
+export default function FavList() {
     const dispatch = useDispatch();
     const favoriteList = useSelector(store => store.favoriteList);
 
     useEffect(() => {
-        dispatch({type: "FETCH_FAVS"});
-        alert(JSON.stringify(favoriteList));
+        dispatch({ type: "FETCH_FAVS" });
     }, []);
-    
+
     return (
         <>
-        <h1>IN FavList</h1>
-        <ul>{favoriteList.length === 0 ? <h2>Loading...</h2> : 
-        favoriteList.map(gif => (
-            <li key={gif.id}><img src={gif.gif_url} /></li>
-        ))}</ul>
+            <h1>IN FavList</h1>
+            <ul>{favoriteList.length === 0 ? <h2>Loading...</h2> :
+                favoriteList.map(gif => (
+                    <div class="w3-dropdown-hover">
+                        <li key={gif.id}><img src={gif.gif_url} /></li>
+                        <div>
+                            <button className="w3-button">Assign Catgory</button>
+                            <div className="w3-dropdown-content w3-bar-block w3-border">
+                                <a href="#" class="w3-bar-item w3-button">Wild</a>
+                                <a href="#" class="w3-bar-item w3-button">Uproarious</a>
+                                <a href="#" class="w3-bar-item w3-button">Poignant</a>
+                                <a href="#" class="w3-bar-item w3-button">Felicitous</a>
+                                <a href="#" class="w3-bar-item w3-button">Whimsical</a>
+                            </div>
+                        </div>
+                    </div>
+                ))}</ul>
         </>
     )
 }
