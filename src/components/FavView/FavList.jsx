f1a8c250140957eaf24123328afd76ab2a3c8f58
Andrew Haarsager <athaarsager@gmail.com>
2024-02-08 14:54:42 -0600

    Created map function in FavList.jsx

--- src/components/FavView/FavList.jsx
+++ src/components/FavView/FavList.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useSelector } from "react";
 
 export default function FavList () {
+    const favoriteList = useSelector(store => store.favoriteList);
     
     return (
         <>
         <h1>IN FavList</h1>
-        <ul></ul>
+        <ul>{favoriteList.map(gif => (
+            <li key={gif.id}><img src={gif.gif_url} /></li>
+        ))}</ul>
         </>
     )
 }
